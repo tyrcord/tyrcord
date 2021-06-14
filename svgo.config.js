6a52fbf1f3c8d81a4411390bf586dc2997c79074
Thibault Zanini <thibaultzanini@gmail.com>
2021-06-14 16:42:31 +0200

    update intro graphic

---
+++ svgo.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    'cleanupAttrs',
+    'removeDoctype',
+    'removeComments',
+    'removeUselessDefs',
+    'removeEmptyAttrs',
+    'removeEmptyContainers',
+    'removeMetadata',
+    'removeTitle',
+    'removeDesc',
+    'mergePaths',
+    'convertShapeToPath',
+    'collapseGroups',
+    'cleanupNumericValues',
+    'removeUnusedNS',
+    'removeNonInheritableGroupAttrs',
+    'convertPathData',
+    'convertColors'
+  ],
+};
