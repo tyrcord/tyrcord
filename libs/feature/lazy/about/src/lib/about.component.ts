374550a8a9834e7f671cbacb7bf7b56358ce00f4
Thibault Zanini <thibaultzanini@gmail.com>
2021-06-15 16:58:49 +0200

    Refactor About Page

--- libs/feature/lazy/about/src/lib/about.component.ts
+++ libs/feature/lazy/about/src/lib/about.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'tyrcord-feature-lazy-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  startDate = DateTime.fromISO('2013-06-01T00:00:00Z');
+
+  workingYears: number;
+
+  ngOnInit(): void {
+    const { years } = DateTime.now().diff(this.startDate, 'years').toObject();
+    this.workingYears = Math.floor(years);
+  }
+}
