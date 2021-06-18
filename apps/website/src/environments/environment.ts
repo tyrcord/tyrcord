52c3672c0e649460be731c029c739ca3f06a207a
Thibault Zanini <thibaultzanini@gmail.com>
2021-06-18 13:18:25 +0200

    update firebase config

--- apps/website/src/environments/environment.ts
+++ apps/website/src/environments/environment.ts
@@ -1,4 +1,10 @@
+import { firebaseConfig } from './firebase.config';
+
 export const environment = {
   production: false,
-  firebase: {},
+  firebase: {
+    ...firebaseConfig,
+    databaseURL: 'http://localhost:9000?ns=tyrcord-816d7',
+    ssl: false,
+  },
 };
