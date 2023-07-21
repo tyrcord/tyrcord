cecfc3d7cb5f453760fb940470dcff2664caafef
Thibault Zanini <thibaultzanini@gmail.com>
2023-07-21 11:11:19 +0200

    feat: migrate to nx 16

--- libs/feature/lazy/secret/src/lib/guards/gatekeeper.guard.ts
+++ libs/feature/lazy/secret/src/lib/guards/gatekeeper.guard.ts
@@ -1,11 +1,11 @@
-import { CanActivate, UrlTree, Router } from '@angular/router';
+import { UrlTree, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { EasterEggService } from '@tyrcord/feature/shared/logic/services';
 
 @Injectable()
-export class Gatekeeper implements CanActivate {
+export class Gatekeeper  {
   constructor(private router: Router) {}
 
   canActivate():
